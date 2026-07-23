917eb4ed0b0bdb7452488c15b6cb82f5e7198d08
TerryCurranZellis <terry.curran@zellis.com>
2026-07-23 16:55:24 +0100

    updated licence text

--- src/main/java/com/towermarsh/opendata/config/model/BootstrapConfig.java
+++ src/main/java/com/towermarsh/opendata/config/model/BootstrapConfig.java
@@ -1,3 +1,27 @@
+/*
+ * Filename: BootstrapConfig.java
+ *
+ * (c) Copyright 2026 Terry Curran
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * The author may be contacted by email to the following address:
+ *
+ * terry.curran@towermarsh.co.uk
+ */
+
 package com.towermarsh.opendata.config.model;
 
 import java.nio.file.Path;
@@ -13,6 +37,9 @@ import java.util.Objects;
  * @param archiveDirectory archive directory
  * @param failedDirectory failed-download directory
  * @param values additional bootstrap values
+ *
+ * @author Terry Curran
+ * @version 21 Jul 2026
  */
 public record BootstrapConfig(
         String applicationName,
--- src/main/java/com/towermarsh/opendata/config/model/HttpMethod.java
+++ src/main/java/com/towermarsh/opendata/config/model/HttpMethod.java
@@ -1,7 +1,34 @@
+/*
+ * Filename: HttpMethod.java
+ *
+ * (c) Copyright 2026 Terry Curran
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * The author may be contacted by email to the following address:
+ *
+ * terry.curran@towermarsh.co.uk
+ */
+
 package com.towermarsh.opendata.config.model;
 
 /**
  * Supported HTTP request methods.
+ *
+ * @author Terry Curran
+ * @version 21 Jul 2026
  */
 public enum HttpMethod {
     GET,
